5e21a2f59278c15d9da07dd3e083462625150008
BxptisteM <baptiste.moreau@epitech.eu>
2025-03-11 14:47:39 +0100

    feat(backend): Initialized backend app

---
+++ back/src/main/java/com/bxptistem/QuickNotes_JA/QuickNotesJaApplication.java
@@ -0,0 +1,13 @@
+package com.bxptistem.QuickNotes_JA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickNotesJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickNotesJaApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/bxptistem/QuickNotes_JA/QuickNotesJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bxptistem.QuickNotes_JA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickNotesJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
